296218701d8fbcf4c06a49a09e1f8640e68acef7
SOM3-1 <dush.gowda@gmail.com>
2025-03-12 20:21:49 -0500

    misc

--- src/components/reusable/Intro.tsx
+++ src/components/reusable/Intro.tsx
@@ -9,7 +9,7 @@ export const Intro: React.FC<{type: ScreenType}> = ({type}) => {
 
   const subtitle:string = type !== ScreenType.TRANSACTIONS ?
   'Take control of your finances. Start planning your budget today!' : 
-  'Start tracking your expenses to gain valuable insights into your spending habits';
+  'Start tracking your expenses to gain valuable insights into your spending habits.';
 
   const [name, setName] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
